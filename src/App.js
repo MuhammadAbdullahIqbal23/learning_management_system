0fae3412af02b84aea9adf7d6063823873e8f61e
MuhammadAbdullahIqbal23 <112970908+MuhammadAbdullahIqbal23@users.noreply.github.com>
2024-12-09 05:31:19 +0500

    final

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Dashboard from './panels/admin/pages/Admin/Dashboard';
-import Login from './panels/admin/pages/Auth/Login';
-import Register from './panels/admin/pages/Auth/Register';
-import ManageUsers from './panels/admin/pages/Admin/ManageUsers';
-import ManageCourses from './panels/admin/pages/Admin/ManageCourses';
-import StudentCoursesDashboard from './panels/admin/pages/Admin/studentCourseEnrollment';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import AppRoutes from './routes';
 
-const App = () => {
+function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/admin" element={<Dashboard />}>
-            <Route index element={<Navigate to="/admin/dashboard" />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="manageusers" element={<ManageUsers />} />
-            <Route path="courses" element={<ManageCourses />} />
-            <Route path="enrolled" element={<StudentCoursesDashboard />} />
-          </Route>
-          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
-        </Routes>
-      </div>
+      <MainLayout>
+        <AppRoutes />
+      </MainLayout>
     </Router>
   );
-};
+}
 
 export default App;
